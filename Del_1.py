4456a05b06cbcc6ccb1b4bb1b655d361d5992485
Mehdi <Mehdi.mz@hotmail.com>
2025-03-17 20:28:46 +0100

    Done

--- Del_1.py
+++ Del_1.py
@@ -10,15 +10,4 @@ dataframe = pd.DataFrame(data)
 latex_output = dataframe.describe().style.format(precision=3).to_latex()
 print(latex_output)
 
-variables = ["title", "house", "noble", "age", "male"]
-
-for var in variables:
-    plt.figure(figsize=(8, 6))
-    plt.hist(dataframe[var], bins=20, color='red', edgecolor='black', alpha=0.7)
-    plt.title(f'Histogram av {var}', fontsize=14)
-    plt.xlabel(var, fontsize=12)
-    plt.ylabel('Antall tilfeller', fontsize=12)
-    plt.grid(axis='y', linestyle='--', alpha=0.7)
-    plt.show()
-
 
